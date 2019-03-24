f275cecfed84faa4d7d10afd8a7b4f777b8925ba
Abel Muiño <amuino@gmail.com>
2019-03-24 22:43:45 +0100

    Remove 1-by-1 case. Read keys and time from ARGV

--- bench.exs
+++ bench.exs
@@ -1,22 +1,20 @@
 Repo.start_link()
 
-keys = 1..1_000_000 |> Enum.map(&Integer.to_string/1)
+[num_keys, time] =
+  case System.argv() do
+    [] -> [10_000, 60]
+    [num_keys] -> [String.to_integer(num_keys), 60]
+    [num_keys, seconds] -> [String.to_integer(num_keys), String.to_integer(seconds)]
+    _ -> raise("too many parameters")
+  end
+
+IO.puts(
+  "#{IO.ANSI.green()}Upserting #{num_keys} unique keys for #{time} seconds#{IO.ANSI.reset()}\n"
+)
+
+keys = 1..num_keys |> Enum.map(&Integer.to_string/1)
 
 run_at = NaiveDateTime.utc_now() |> NaiveDateTime.to_iso8601()
-
-upsert_1_by_1 = fn isolation ->
-  keys
-  |> Enum.each(fn key ->
-    Repo.query!("SET TRANSACTION ISOLATION LEVEL #{isolation}")
-
-    Repo.transaction(fn ->
-      Repo.insert(
-        %BenchSchema{key: key, value: run_at},
-        on_conflict: :replace_all_except_primary_key
-      )
-    end)
-  end)
-end
 
 upsert_in_chunks = fn chunk_size, isolation ->
   keys
@@ -48,18 +46,19 @@ upsert_in_chunks = fn chunk_size, isolation ->
 end
 
 jobs = %{
-  "no chunks REPEATABLE READ (default)" => fn -> upsert_1_by_1.("REPEATABLE READ") end,
-  "no chunks READ UNCOMMITTED (lowest)" => fn -> upsert_1_by_1.("READ UNCOMMITTED") end,
-  "1x   REPEATABLE READ (default)" => fn -> upsert_in_chunks.(1, "REPEATABLE READ") end,
-  "10x  REPEATABLE READ (default)" => fn -> upsert_in_chunks.(10, "REPEATABLE READ") end,
-  "100x REPEATABLE READ (default)" => fn -> upsert_in_chunks.(100, "REPEATABLE READ") end,
-  "1000x REPEATABLE READ (default)" => fn -> upsert_in_chunks.(1000, "REPEATABLE READ") end,
-  "1x   READ UNCOMMITTED (lowest)" => fn -> upsert_in_chunks.(1, "READ UNCOMMITTED") end,
-  "10x  READ UNCOMMITTED (lowest)" => fn -> upsert_in_chunks.(10, "READ UNCOMMITTED") end,
-  "100x READ UNCOMMITTED (lowest)" => fn -> upsert_in_chunks.(100, "READ UNCOMMITTED") end,
-  "1000x READ UNCOMMITTED (lowest)" => fn -> upsert_in_chunks.(1000, "READ UNCOMMITTED") end
+  "1x   REPEATABLE READ (default)" => fn _ -> upsert_in_chunks.(1, "REPEATABLE READ") end,
+  "10x  REPEATABLE READ (default)" => fn _ -> upsert_in_chunks.(10, "REPEATABLE READ") end,
+  "100x REPEATABLE READ (default)" => fn _ -> upsert_in_chunks.(100, "REPEATABLE READ") end,
+  "1000x REPEATABLE READ (default)" => fn _ -> upsert_in_chunks.(1000, "REPEATABLE READ") end,
+  "1x   READ UNCOMMITTED (lowest)" => fn _ -> upsert_in_chunks.(1, "READ UNCOMMITTED") end,
+  "10x  READ UNCOMMITTED (lowest)" => fn _ -> upsert_in_chunks.(10, "READ UNCOMMITTED") end,
+  "100x READ UNCOMMITTED (lowest)" => fn _ -> upsert_in_chunks.(100, "READ UNCOMMITTED") end,
+  "1000x READ UNCOMMITTED (lowest)" => fn _ -> upsert_in_chunks.(1000, "READ UNCOMMITTED") end
 }
 
-Benchee.run(jobs, formatters: [Benchee.Formatters.Console], time: 10 * 60)
-
-IO.inspect(Repo.query!("select count(*) from bench"))
+Benchee.run(
+  jobs,
+  formatters: [Benchee.Formatters.Console],
+  time: time,
+  before_each: fn _ -> Repo.delete_all(BenchSchema) end
+)
